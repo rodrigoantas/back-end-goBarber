6cb1a2db2847de18e91c3faa102d108a10749c40
Rodrigo Antas <rodr.antas@gmail.com>
2020-10-29 10:13:06 -0300

    módulo 1 e 2, feito até a autenticação de usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '52b1abf52c5c68fd626fd2d294f81f51',
+    expiresIn: '1d',
+  },
+};
---
+++ src/models/Appointment.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // não precisa mais desse constructor, quando se cria uma entidade com typeorm, o constructor é meio que criado de forma automática, não vamos mais criar o model da maneira tradicional com New Appointment.
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
+}
+
+export default Appointment;

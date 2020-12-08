46a46abdd2c7d51d292d6502f24873b55cfc36cc
Rodrigo Antas <rodr.antas@gmail.com>
2020-12-08 19:27:00 -0300

    testing

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-// import { parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -9,7 +9,7 @@ export default class AppointmentsController {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
-    // const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
     console.log({
       date,
@@ -20,7 +20,7 @@ export default class AppointmentsController {
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date,
+      date: parsedDate,
       provider_id,
       user_id,
     });
